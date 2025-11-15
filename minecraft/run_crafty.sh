6b688317d56e473da6320c0e81762ca8591768fa
srPudinoso <50886133+srPudinoso@users.noreply.github.com>
2025-11-15 01:50:55 +0000

    add server files

---
+++ minecraft/run_crafty.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /workspaces/server/minecraft
+source .venv/bin/activate 
+cd crafty-4 
+exec python3 main.py 
---
+++ minecraft/run_crafty_service.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /workspaces/server/minecraft
+source .venv/bin/activate 
+cd crafty-4 
+python3 main.py -d
